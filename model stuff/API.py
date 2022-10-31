5275fa26a6e645bee73f56e6b4bad5eb42a7bb53
erinhorbacz <108021143+erinhorbacz@users.noreply.github.com>
2022-10-31 14:47:08 -0400

    Add files via upload
    Code for creating book database. Uses openlibrary api

--- model stuff/API.py
+++ model stuff/API.py
@@ -1,10 +1,26 @@
 from internetarchive import get_item, search_items
 import urllib.request
 import json
+import pandas as pd
+from langdetect import detect, detect_langs
+import re
 
 # OL26491056M
 # OL45804W
 
+
+def language_detection(text, method = "single"):
+    try:
+        if(method.lower() != "single"):
+            result = detect_langs(text)
+
+        else:
+            result = detect(text)
+    except:
+        print("exception occurred")
+        print(text)
+        return -1;
+    return result
 
 
 def get_descriptions(works):
@@ -14,17 +30,27 @@ def get_descriptions(works):
         title = work["title"]
         url = 'https://openlibrary.org{}.json'.format(identifier)
         httpresponse = urllib.request.urlopen(url)
-        # .decode('utf8')
-
         data_json = json.loads(httpresponse.read())
         # print(data_json["description"])
         keys = data_json.keys()
         if "description" in keys:
-            description = data_json["description"]
-            print("DID THIS ONE")
-            data.append({"Title": title, "ID": identifier, "Description": description})
-
-    return data
+            # print(title)
+            # print(identifier)
+            if type(data_json["description"]) == dict:
+                description = data_json["description"]["value"]
+            else:
+                descriptions = data_json["description"]
+            print(description)
+            print(type(description))
+            print("\n\n\n\n")
+            if bool(re.match('^(?=.*[a-zA-Z])', description)):
+                lang = (language_detection(description))
+                if lang == "en":
+                    # print(description)
+                    # print("\n\n\n\n")
+                    data.append({"Title": title, "ID": identifier, "Description": description})
+    df = pd.DataFrame(data)
+    return df
 
 
 
@@ -36,25 +62,22 @@ def get_list_of_works(date = False, year = 2020, language = False):
     if language:
         # search for books in english
         # https://openlibrary.org/search.json?q=language:eng
-        url = "https://openlibrary.org/search.json?q=language:eng"
+        url = "https://openlibrary.org/search.json?q=language:eng&limit=10000"
 
     httpresponse = urllib.request.urlopen(url)
-    # .decode('utf8')
-
     works = []
 
     data_json = json.loads(httpresponse.read())
-    print(len(data_json["docs"]))
-    for item in data_json:
-        print(data_json["numFound"])
-        if item == "docs":
-            # print(type(data_json[item]))
-            for element in data_json[item]:
-                print(element["key"])
-                works.append({"identifier": element["key"], "title": element["title"]})
+    # print(len(data_json["docs"]))
+    for element in data_json["docs"]:
+        if "language" in element:
+            # print(element["language"])
+            works.append({"identifier": element["key"], "title": element["title"]})
     return get_descriptions(works)
 
 
-works = get_list_of_works(language = True)
-# print(type(works))
-# print(works)
+data = get_list_of_works(language = True)
+data.to_csv('book_data_eng.csv')
+print("ROWS: ", data.shape[0])
+# print(len(data))
+# print(data)
