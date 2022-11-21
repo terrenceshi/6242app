7d4dd1faa6e670461bf5fa3c997f9280f672326b
erinhorbacz <108021143+erinhorbacz@users.noreply.github.com>
2022-11-21 13:09:50 -0500

    Erin Update App.js
    updated with my code

--- b2nfrontend/src/App.js
+++ b2nfrontend/src/App.js
@@ -1,27 +1,38 @@
 import './App.css';
 import * as React from 'react';
+
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import Home from "./Home"
 import Issue from "./Issue"
-import {TextField, Box, Button } from '@mui/material';
+import Nav from "./Nav.js";
+import About from "./About.js";
+
+import { Typography, TextField, Box, Button } from '@mui/material';
 
 //each textfield has attributes value and defaultvalue which can be used if we want our page to open with an example
 
+
+
 function App() {
-  
+  //const [value, setValue] = React.useState('Controlled');
+
+
   return (
     <div className="App">
-      <h1 className="title">
-        From Books to Nooks
-      </h1>
-      <h5 className="subtitle">
-      	Curated playlists from your favorite book.
-      </h5>
+    <Nav />
+      <Typography variant="h1">
+        From books to nooks
+      </Typography>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/Issue" element={<Issue />} />
+        <Route path="/About" element={<About />} />
       </Routes>
+
     </div>
+
   );
 }
 
