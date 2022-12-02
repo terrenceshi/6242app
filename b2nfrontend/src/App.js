af464c715e391dd99b139fea31af99ea31d64691
Terrence Shi <terrenceshi@gmail.com>
2022-12-02 04:33:24 -0500

    ad

--- b2nfrontend/src/App.js
+++ b2nfrontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import * as React from 'react';
 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Home from "./Home"
 import Issue from "./Issue"
 import Nav from "./Nav.js";
